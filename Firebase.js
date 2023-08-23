f580d2c09586dad767aa625bb539bc62bd9cd4a2
Hydra_z4l(Myat Myat Htun) <125865996+Htoosansain@users.noreply.github.com>
2023-08-23 16:48:18 +0630

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+let config = {
+  apiKey: "AIzaSyBGKTkUsgOc3Azmas8srS6lwR1iFoHSoWA",
+  authDomain: "mahtrat.firebaseapp.com",
+  projectId: "mahtrat",
+  storageBucket: "mahtrat.appspot.com",
+  messagingSenderId: "1099178230425",
+  appId: "1:1099178230425:web:29e69b346a108c4505ae08",
+  measurementId: "G-62PDFEWHK7"
+
+
+
+};
+
+firebase.initializeApp(config);
